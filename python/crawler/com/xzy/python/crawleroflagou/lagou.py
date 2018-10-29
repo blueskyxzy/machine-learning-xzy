057e80345d53996c379e49bed8ee7b9b955fb3ff
blueskyxzy <blueskyxzy@gmail.com>
2018-10-29 11:27:13 +0800

    拉勾网爬虫修改

--- python/crawler/com/xzy/python/crawleroflagou/lagou.py
+++ python/crawler/com/xzy/python/crawleroflagou/lagou.py
@@ -11,10 +11,26 @@ from wordcloud import WordCloud
 
 # 根据拉勾网接口查询部分招聘信息，绘图总结和数据分析
 
+# 实际抓到的接口：https://www.lagou.com/jobs/positionAjax.json?city=%E6%9D%AD%E5%B7%9E&needAddtionalResult=false
+# Request Method: POST
+# 删减后的返回：
+# {"success":true,"requestId":null,"resubmitToken":null,"msg":null,
+# "content":{"pageNo":1,"pageSize":15,"hrInfoMap":{"2478263":{"userId":1648814,"positionName":"人力资源","phone":null,"receiveEmail":null,"realName":"大禹信息","portrait":"i/image/M00/35/D7/CgqKkVdcsXCAVPtyAAAXRqiHOqA422.jpg","canTalk":true,"userLevel":"G1"}},"positionResult":{"totalCount":3891,"locationInfo":{"city":"杭州","district":null,"businessZone":null,"locationCode":null,"isAllhotBusinessZone":false,"queryByGisCode":false},"resultSize":15,"queryAnalysisInfo":{"positionName":"java","jobNature":null,"companyName":null,"industryName":null,"usefulCompany":false},"strategyProperty":{"name":"dm-csearch-useUserAllInterest","id":1},"hotLabels":null,"hiTags":null,
+# "result":[
+# {"companyId":198547,"longitude":"120.095509","latitude":"30.307858","positionName":"Java开发工程师（应届生）","workYear":"1年以下","education":"本科","jobNature":"全职","companyLogo":"i/image/M00/22/FF/CgpEMlkRoPqALuE4AAAnYuCr79w664.png","industryField":"移动互联网,数据服务","financeStage":"不需要融资","companySize":"15-50人","city":"杭州","salary":"5k-7k","positionId":5202073,"positionAdvantage":"技术大牛,氛围融洽,五险一金","companyShortName":"酷汇网络",
+#   "createTime":"2018-10-29 09:33:14","district":"西湖区","score":0,"approve":1,"positionLables":["后端"],"industryLables":[],"publisherId":7830411,"companyLabelList":["午餐补助","定期体检","绩效奖金","带薪年假"],
+#   "businessZones":["古墩路"],"formatCreateTime":"09:33发布","deliver":0,"gradeDescription":null,"promotionScoreExplain":null,"firstType":"开发|测试|运维类","secondType":"后端开发","isSchoolJob":0,"subwayline":null,"stationname":null,"linestaion":null,"thirdType":"Java","skillLables":["后端"],"hitags":null,"resumeProcessRate":61,"resumeProcessDay":1,"imState":"disabled","lastLogin":1540775508000,"explain":null,"plus":null,"pcShow":0,"appShow":0,"companyFullName":"杭州酷汇网络技术有限公司","adWord":0
+# },
+# {"companyId":286594,"longitude":"120.045424","latitude":"30.291923","positionName":"Java工程师","workYear":"不限","education":"本科","jobNature":"全职","companyLogo":"images/logo_default.png","industryField":"金融,数据服务","financeStage":"上市公司","companySize":"500-2000人","city":"杭州","salary":"12k-24k","positionId":4492083,"positionAdvantage":"Java,人工智能,互联网金融","companyShortName":"同花顺",
+#   "createTime":"2018-10-29 09:16:48","district":"余杭区","score":0,"approve":0,"positionLables":["视频","系统架构"],"industryLables":["视频","系统架构"],"publisherId":9589848,"companyLabelList":["\"\""],
+#   "businessZones":null,"formatCreateTime":"09:16发布","deliver":0,"gradeDescription":null,"promotionScoreExplain":null,"firstType":"开发|测试|运维类","secondType":"后端开发","isSchoolJob":0,"subwayline":null,"stationname":null,"linestaion":null,"thirdType":"Java","skillLables":["系统架构"],"hitags":null,"resumeProcessRate":0,"resumeProcessDay":0,"imState":"today","lastLogin":1540775735000,"explain":null,"plus":null,"pcShow":0,"appShow":0,"companyFullName":"浙江核新同花顺网络信息股份有限公司","adWord":0},]}
+# },
+# "code":0
+# }
+
 
 # 接口查询获取json数据
 def getjson(queryCode):
-
     param = {
 
     }
